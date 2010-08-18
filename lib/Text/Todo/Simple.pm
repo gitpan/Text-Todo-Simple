bd086ede37480988b3ee102bc0d1824df6218b5f
Alessandro Ghedini <alexbio@cpan.org>
2010-08-18 14:53:14 +0000

    Import of ALEXBIO/Text-Todo-Simple-0.03 from CPAN.
    gitpan-cpan-distribution: Text-Todo-Simple
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ALEXBIO/Text-Todo-Simple-0.03.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

--- lib/Text/Todo/Simple.pm
+++ lib/Text/Todo/Simple.pm
@@ -11,11 +11,11 @@ Text::Todo::Simple - Todo list manager that helps in finishing tasks, not organi
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
